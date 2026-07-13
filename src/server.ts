cf337e3ac94118a57a4a30be69dbba7f365d0031
Caua-Vieira <cauasilva8@gmail.com>
2026-07-12 23:25:57 -0300

    feat: create the initial project structure

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+import { AppDataSource } from "./infrastructure/config/data-source";
+
+const PORT = process.env.PORT || 3333;
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Banco conectado!");
+        app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+    })
+    .catch((err) => console.error("Erro ao conectar no banco:", err));
