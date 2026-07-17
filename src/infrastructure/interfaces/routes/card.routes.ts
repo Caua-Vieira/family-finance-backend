ac15d55e6b129ab9487bdb5a93ee5d9a3513931a
Caua-Vieira <cauasilva8@gmail.com>
2026-07-17 12:46:15 -0300

    fix: adjust post cards route

--- src/infrastructure/interfaces/routes/card.routes.ts
+++ src/infrastructure/interfaces/routes/card.routes.ts
@@ -7,7 +7,7 @@ export const cardRoutes = (): Router => {
     const router = Router();
     const cardController = Container.get(CardController);
 
-    router.post("/register", authMiddleware, (req, res) => cardController.register(req, res));
+    router.post("/", authMiddleware, (req, res) => cardController.register(req, res));
     router.get("/", authMiddleware, (req, res) => cardController.list(req, res));
     router.put("/:id", authMiddleware, (req, res) => cardController.update(req, res));
     router.delete("/:id", authMiddleware, (req, res) => cardController.delete(req, res));
